18b19533e8a5215c440a107f7a82c3da0572609f
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-31 06:37:53 +1100

    update total points state for each character in RoundsContainer component

--- src/components/CharacterScore/CharacterScore.jsx
+++ src/components/CharacterScore/CharacterScore.jsx
@@ -1,14 +1,22 @@
 import {StyledCharacterScore} from './CharacterScore.styled';
 import getRandom from "../../utils/randomNumberGenerator";
-import { useRef } from 'react';
+import { useRef, useEffect } from 'react';
 
-function CharacterScore({ char, spells }){
+function CharacterScore({ char, spells, onAddPoints, isComplete }){
     const totalPoints = useRef(0);
 
     const randomIndex = getRandom(0, spells.length - 1);
     const randomSpell = spells[randomIndex];
     console.log("random spell is: ", randomSpell);
     totalPoints.current = totalPoints.current + randomSpell.points;
+    
+    useEffect(() =>{
+        if(isComplete){
+            console.log("It is complete!");
+            onAddPoints(totalPoints.current);
+        }
+    }, [isComplete]);
+    
 
     return (
         <StyledCharacterScore>
--- src/components/RoundsContainer/RoundsContainer.jsx
+++ src/components/RoundsContainer/RoundsContainer.jsx
@@ -1,24 +1,48 @@
 import { StyledRoundsContainer } from "./RoundsContainer.styled";
-import {CharacterScore} from "../../components";
-import { useState } from "react";
+import { CharacterScore } from "../../components";
+import { useState, useEffect } from "react";
 
 function RoundsContainer({ charOne, charTwo, spells }) {
   const [round, setRound] = useState(1);
   const [isComplete, setIsComplete] = useState(false);
+  const [charOnePoints, setCharOnePoints] = useState(0);
+  const [charTwoPoints, setCharTwoPoints] = useState(0);
 
-  function handleNextRoundClick(){
-    if (round < 3)
-        setRound(prevState => ++prevState);
+  useEffect(() => {
+    if (round === 3){
+        console.log("round is: ", round);
+        setIsComplete(!isComplete);
+    }
+    console.log("isComplete is: ", isComplete);
+  }, [round])
+
+  function handleNextRoundClick() {
+      setRound((prevState) => ++prevState);
   }
+
+  console.log("charOnePoints is :", charOnePoints);
 
   return (
     <StyledRoundsContainer>
-      <h2>Round {round} of 3</h2>
-      <div>
-        <CharacterScore char={charOne} spells={spells}/>
-        <CharacterScore char={charTwo} spells={spells}/>
-      </div>
-      <button onClick={handleNextRoundClick}>Next round</button>
+          <h2>Round {round} of 3</h2>
+          <div>
+            <CharacterScore
+              char={charOne}
+              spells={spells}
+              onAddPoints={setCharOnePoints}
+              isComplete={isComplete}
+            />
+            <CharacterScore
+              char={charTwo}
+              spells={spells}
+              onAddPoints={setCharTwoPoints}
+              isComplete={isComplete}
+            />
+          </div>
+          {isComplete? <button>Show outcome</button> : <button onClick={handleNextRoundClick}>Next round</button>}
+          <p>{charOnePoints}</p>
+          <p>{charTwoPoints}</p>
+
     </StyledRoundsContainer>
   );
 }
