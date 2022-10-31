7419161d10552342c07cefa930014364c3b7c566
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-10-31 13:45:46 +1100

    show duel outcome

--- src/components/CharacterScore/CharacterScore.jsx
+++ src/components/CharacterScore/CharacterScore.jsx
@@ -4,25 +4,33 @@ import { useRef, useEffect } from 'react';
 
 function CharacterScore({ char, spells, onAddPoints, isComplete }){
     const totalPoints = useRef(0);
+    const randomIndex = useRef(0);
+    const randomSpell = useRef(null);
 
-    const randomIndex = getRandom(0, spells.length - 1);
-    const randomSpell = spells[randomIndex];
-    console.log("random spell is: ", randomSpell);
-    totalPoints.current = totalPoints.current + randomSpell.points;
-    
     useEffect(() =>{
         if(isComplete){
             console.log("It is complete!");
             onAddPoints(totalPoints.current);
         }
     }, [isComplete]);
+
+    // const randomIndex = getRandom(0, spells.length - 1);
+    // const randomSpell = spells[randomIndex];
+    //console.log("random spell is: ", randomSpell);
+
+    if(!isComplete){
+        randomIndex.current = getRandom(0, spells.length - 1);
+        randomSpell.current = spells[randomIndex.current];
+        totalPoints.current = totalPoints.current + randomSpell.current.points;
+        console.log("total points for ", char.name," is ", totalPoints.current);
+    }
     
 
     return (
         <StyledCharacterScore>
             <h3>{char.name}</h3>
-            <p>Spell used: {randomSpell.name}</p>
-            <p>Spell points: {randomSpell.points}</p>
+            <p>Spell used: {randomSpell.current.name}</p>
+            <p>Spell points: {randomSpell.current.points}</p>
             <p>Total points: {totalPoints.current}</p>
         </StyledCharacterScore>
     )
--- src/components/RoundsContainer/RoundsContainer.jsx
+++ src/components/RoundsContainer/RoundsContainer.jsx
@@ -1,48 +1,68 @@
 import { StyledRoundsContainer } from "./RoundsContainer.styled";
 import { CharacterScore } from "../../components";
-import { useState, useEffect } from "react";
+import { useState, useEffect, useRef } from "react";
 
 function RoundsContainer({ charOne, charTwo, spells }) {
   const [round, setRound] = useState(1);
   const [isComplete, setIsComplete] = useState(false);
   const [charOnePoints, setCharOnePoints] = useState(0);
   const [charTwoPoints, setCharTwoPoints] = useState(0);
+  const [showOutcome, setShowOutcome] = useState(false);
+  const outcomeMessage = useRef("");
 
   useEffect(() => {
-    if (round === 3){
-        console.log("round is: ", round);
-        setIsComplete(!isComplete);
+    if (round === 3) {
+      console.log("round is: ", round);
+      setIsComplete(!isComplete);
     }
     console.log("isComplete is: ", isComplete);
-  }, [round])
+  }, [round]);
 
   function handleNextRoundClick() {
-      setRound((prevState) => ++prevState);
+    setRound((prevState) => ++prevState);
   }
 
-  console.log("charOnePoints is :", charOnePoints);
+  function handleShowOutcomeClick() {
+    if (charOnePoints > charTwoPoints) {
+      outcomeMessage.current = `${charOne.name} wins`;
+    } else if (charOnePoints < charTwoPoints) {
+      outcomeMessage.current = `${charTwo.name} wins`;
+    } else {
+      outcomeMessage.current = `It is a tie!`;
+    }
+    setShowOutcome((prevState) => !prevState);
+  }
+
+  //   console.log("charOnePoints is: ", charOnePoints);
+  //   console.log("charTwoPoints is: ", charTwoPoints);
 
   return (
     <StyledRoundsContainer>
-          <h2>Round {round} of 3</h2>
-          <div>
-            <CharacterScore
-              char={charOne}
-              spells={spells}
-              onAddPoints={setCharOnePoints}
-              isComplete={isComplete}
-            />
-            <CharacterScore
-              char={charTwo}
-              spells={spells}
-              onAddPoints={setCharTwoPoints}
-              isComplete={isComplete}
-            />
-          </div>
-          {isComplete? <button>Show outcome</button> : <button onClick={handleNextRoundClick}>Next round</button>}
-          <p>{charOnePoints}</p>
-          <p>{charTwoPoints}</p>
-
+      <h2>Round {round} of 3</h2>
+      <div>
+        <CharacterScore
+          char={charOne}
+          spells={spells}
+          onAddPoints={setCharOnePoints}
+          isComplete={isComplete}
+        />
+        <CharacterScore
+          char={charTwo}
+          spells={spells}
+          onAddPoints={setCharTwoPoints}
+          isComplete={isComplete}
+        />
+      </div>
+      {isComplete ? (
+        <button onClick={handleShowOutcomeClick}>Show outcome</button>
+      ) : (
+        <button onClick={handleNextRoundClick}>Next round</button>
+      )}
+      {showOutcome ? (
+        <div>
+          <p>{outcomeMessage.current}</p>
+        </div>
+      ) : null}
     </StyledRoundsContainer>
   );
 }
