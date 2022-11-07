46d72778ce72aeffa5f8e0343053642471dd3e06
LaveeshkaMahadea <laveeshka94@gmail.com>
2022-11-07 13:54:35 +1100

    swap Redirect for Link component to enhance UX

--- src/components/Duel/Duel.jsx
+++ src/components/Duel/Duel.jsx
@@ -7,17 +7,30 @@ function Duel({ isChosen, charOne, charTwo, spells }) {
   const match = useRouteMatch();
   console.log(match);
   //if the user has not chosen two characters yet, redirect them to the home page
-  if (!isChosen) return <Redirect to="/" />;
+  // if (!isChosen) return <Redirect to="/" />;
+  //However, using Redirect isn't user friendly; the user does not know why they are being re-directed. Instead, show a Link for re-direction
 
   return (
-    <StyledDuel>
-      <ChosenCharacters charOne={charOne} charTwo={charTwo} />
-      <Link to={"/duel/start"}>Start Duel!</Link>
-      {/* we use the current URL from the `match` object as part of the path; this will generate a url "/duel/start" */}
-      <Route path={`${match.url}/start`}>
-        <RoundsContainer charOne={charOne} charTwo={charTwo} spells={spells}/>
-      </Route>
-    </StyledDuel>
+    <>
+      {isChosen ? (
+        <StyledDuel>
+          <ChosenCharacters charOne={charOne} charTwo={charTwo} />
+          <Link to={"/duel/start"}>Start Duel!</Link>
+          {/* we use the current URL from the `match` object as part of the path; this will generate a url "/duel/start" */}
+          <Route path={`${match.url}/start`}>
+            <RoundsContainer
+              charOne={charOne}
+              charTwo={charTwo}
+              spells={spells}
+            />
+          </Route>
+        </StyledDuel>
+      ) : (
+        <Link exact to={"/"}>
+          Choose two wizards first!
+        </Link>
+      )}
+    </>
   );
 }
 
